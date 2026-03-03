8d9997fe8633376c415c6e2bd7f775b7cba4fe0f
iNtERRRnet2 <andrejs_nesterovics@inbox.lv>
2026-03-03 12:43:43 +0200

    Andrejs Ņesterovičs

--- cypress/e2e/juice-shop.cy.js
+++ cypress/e2e/juice-shop.cy.js
@@ -1,4 +1,6 @@
-import { HomePage } from "../pageObjects/HomePage";
+import { HomePage } from "../pageObjects/homePage";
+import { LoginPage } from "../pageObjects/loginPage";
+import { RegistrationPage } from "../pageObjects/registrationPage";
 
 describe("Juice-shop scenarios", () => {
   context("Without auto login", () => {
@@ -10,31 +12,55 @@ describe("Juice-shop scenarios", () => {
 
     it("Login", () => {
       // Click Account button
+      HomePage.accountButton.click();
       // Click Login button
+      HomePage.loginButton.click();
       // Set email value to "demo"
+      LoginPage.emailField.type("demo");
       // Set password value to "demo"
+      LoginPage.passwordField.type("demo");
       // Click Log in
+      LoginPage.loginButton.click();
       // Click Account button
+      HomePage.accountButton.click();
       // Validate that "demo" account name appears in the menu section
+      HomePage.userProfileButton.should('contain.text', 'demo');
     });
 
     it("Registration", () => {
       // Click Account button
-      // Login button
+      HomePage.accountButton.click();
+      // Login button9
+      HomePage.loginButton.click();
       // Click "Not yet a customer?"
+      LoginPage.notYetCustomerButton.click();
       // Find - how to generate random number in JS
       // Use that number to genarate unique email address, e.g.: email_7584@ebox.com
+      const email = `email_${Math.floor(Math.random() * 10000)}@ebox.com`;
+      const password = 'randomPassword34950';
       // Save that email address to some variable
+      RegistrationPage.emailField.type(email);
       // Fill in password field and repeat password field with same password
+      RegistrationPage.passwordField.type(password);
+      RegistrationPage.repeatPasswordField.type(password);
       // Click on Security Question menu
+      RegistrationPage.sequrityQuestionMenu.click();
       // Select  "Name of your favorite pet?"
+      RegistrationPage.dropdownMenuOptions.contains("Name of your favorite pet?").click();
       // Fill in answer
+      RegistrationPage.securityAnswerField.type("Dog");
       // Click Register button
+      RegistrationPage.registrationButton.click();
       // Set email value to previously created email
+      LoginPage.emailField.type(email);
       // Set password value to previously used password value
+      LoginPage.passwordField.type(password);
       // Click login button
+      LoginPage.loginButton.click();
       // Click Account button
+      HomePage.accountButton.click();
       // Validate that account name (with previously created email address) appears in the menu section
+      HomePage.userProfileButton.should('contain.text', email);
     });
   });
 
@@ -46,51 +72,91 @@ describe("Juice-shop scenarios", () => {
 
     it("Search and validate Lemon", () => {
       // Click on search icon
+      HomePage.searchButton.click();
       // Search for Lemon
+      HomePage.searchField.type("Lemon{enter}");
       // Select a product card - Lemon Juice (500ml)
+      HomePage.productCards.contains("Lemon Juice (500ml)").click();
       // Validate that the card (should) contains "Sour but full of vitamins."
+      HomePage.productCard.should("contain.text", "Sour but full of vitamins.")
     });
 
     // Create scenario - Search 500ml and validate Lemon, while having multiple cards
+    it("Search 500ml and validate Lemon, while having multiple cards", () => {
     // Click on search icon
+    HomePage.searchButton.click();
     // Search for 500ml
+    HomePage.searchField.type("500ml{enter}");
     // Select a product card - Lemon Juice (500ml)
+    HomePage.productCards.contains("Lemon Juice (500ml)").click();
     // Validate that the card (should) contains "Sour but full of vitamins."
+    HomePage.productCard.should("contain.text", "Sour but full of vitamins.")
+    })
 
     // Create scenario - Search 500ml and validate cards
+    it("Search 500ml and validate cards", () => {
     // Click on search icon
+    HomePage.searchButton.click();
     // Search for 500ml
+    HomePage.searchField.type("500ml{enter}");
     // Select a product card - Eggfruit Juice (500ml)
+    HomePage.productCards.contains("Eggfruit Juice (500ml)").click();
     // Validate that the card (should) contains "Now with even more exotic flavour."
+    HomePage.productCard.should("contain.text", "Now with even more exotic flavour.");
     // Close the card
+    HomePage.closeCardButton.click();
     // Select a product card - Lemon Juice (500ml)
+    HomePage.productCards.contains("Lemon Juice (500ml)").click();
     // Validate that the card (should) contains "Sour but full of vitamins."
+    HomePage.productCard.should("contain.text", "Sour but full of vitamins.")
     // Close the card
+    HomePage.closeCardButton.click();
     // Select a product card - Strawberry Juice (500ml)
+    HomePage.productCards.contains("Strawberry Juice (500ml)").click();
     // Validate that the card (should) contains "Sweet & tasty!"
+    HomePage.productCard.should("contain.text", "Sweet & tasty!")
+    })
 
     // Create scenario - Read a review
+    it("Read a review", () => {
     // Click on search icon
+    HomePage.searchButton.click();
     // Search for King
+    HomePage.searchField.type("King{enter}");
     // Select a product card - OWASP Juice Shop "King of the Hill" Facemask
+    HomePage.productCards.contains('OWASP Juice Shop "King of the Hill" Facemask').click();
     // Click expand reviews button/icon (wait for reviews to appear)
+    HomePage.expandReviews.click();
     // Validate review - K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!
+    HomePage.reviwBox.should("contains.text", "K33p5 y0ur ju1cy 5plu773r 70 y0ur53lf!");
+    })
 
     // Create scenario - Add a review
+    it("Add a reviw", () => {
     // Click on search icon
+    HomePage.searchButton.click();
     // Search for Raspberry
+    HomePage.searchField.type("Raspberry{enter}");
     // Select a product card - Raspberry Juice (1000ml)
+    HomePage.productCards.contains('Raspberry Juice (1000ml)').click();
     // Type in review - "Tastes like metal"
+    HomePage.reviewTextBox.type("Tastes like metal");
     // Click Submit
+    HomePage.submitButton.click();
     // Click expand reviews button/icon (wait for reviews to appear)
+    HomePage.expandReviews.click();
     // Validate review -  "Tastes like metal"
+    HomePage.reviwBox.should("contains.text", "Tastes like metal");
+    })
 
     // Create scenario - Validate product card amount
+    it.only("Validate product card amount", () => {
     // Validate that the default amount of cards is 12
     // Change items per page (at the bottom of page) to 24
     // Validate that the amount of cards is 24
     // Change items per page (at the bottom of page) to 36
     // Validate that the amount of cards is 35
+    })
 
     // Create scenario - Buy Girlie T-shirt
     // Click on search icon
--- cypress/pageObjects/homePage.js
+++ cypress/pageObjects/homePage.js
@@ -12,4 +12,64 @@ export class HomePage extends BasePage {
   static get meWantItButton() {
     return cy.get("[aria-label='dismiss cookie message']");
   }
+
+  static get accountButton()
+  {
+    return cy.get("#navbarAccount")
+  }
+
+  static get loginButton()
+  {
+    return cy.get("#navbarLoginButton")
+  }
+
+  static get userProfileButton()
+  {
+    return cy.get("button[aria-label='Go to user profile']")
+  }
+
+  static get searchButton()
+  {
+    return cy.get("#searchQuery")
+  }
+
+  static get searchField()
+  {
+    return cy.get("#mat-input-1")
+  }
+
+  static get productCards()
+  {
+    return cy.get("[aria-label='Click for more information about the product']")
+  }
+
+  static get productCard()
+  {
+    return cy.get("[class*='mdc-dialog__content']")
+  }
+
+  static get closeCardButton()
+  {
+    return cy.get("[aria-label='Close Dialog']");
+  }
+
+  static get expandReviews()
+  {
+    return cy.get('[aria-label="Expand for Reviews"]')
+  }
+
+  static get reviwBox()
+  {
+    return cy.get('[class="ng-star-inserted"]')
+  }
+
+  static get reviewTextBox()
+  {
+    return cy.get('[aria-label="Text field to review a product"]')
+  }
+
+  static get submitButton()
+  {
+    return cy.get('#submitButton')
+  }
 }
